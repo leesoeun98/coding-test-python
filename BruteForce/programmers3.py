7f1a75d5aac5a5fb5fab0a57b91e70ff12f8c7a3
soeun lee <thdmsdl87@gmail.com>
2020-09-25 14:13:16 +0900

    노란색 타일의 가로와 세로를 먼저 구한 후 이 것을 이용해 갈색 타일의 개수와 타일이 맞는지 확인한다. 맞을 경우 노란색타일의 가로, 세로+2를 반환한다.

--- BruteForce/programmers3.py
+++ BruteForce/programmers3.py
@@ -1,25 +1,6 @@
-def cal(answer, brown):
-    for carpet in answer:
-        if carpet[1] < 2:
-            tbrown = carpet[0] * carpet[1]
-            if tbrown == brown:
-                return carpet[0], carpet[1]
-        elif carpet[0] < 2:
-            tbrown = carpet[0] * carpet[1]
-            if tbrown == brown:
-                return carpet[0], carpet[1]
-        else:
-            tbrown = 2 * carpet[0] + 2 * (carpet[1] - 2)
-            if tbrown == brown:
-                return [carpet[0], carpet[1]]
-
-
 def solution(brown, yellow):
-    answer = []
-    size = brown + yellow
-    for i in range(1, size+1):
-        for j in range(1, size + 1):
-            if i * j == size and i >= j:
-                answer.append([i, j])
-                break
-    return cal(answer, brown)
+    for yellow_height in range(1, yellow+1):
+        if yellow % yellow_height==0:
+            yellow_width=yellow//yellow_height
+            if brown==(yellow_width)*2+2*(2+yellow_height):
+                return [yellow_width+2, yellow_height+2]
