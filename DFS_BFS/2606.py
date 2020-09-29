7b2581a32418fc7cf322203f1883c384c02b7651
soeun lee <thdmsdl87@gmail.com>
2020-09-29 23:48:34 +0900

    바이러스에 걸린 컴퓨터를 모두 찾기 위해 이전의 BFS 코드 이용. 단, 루트를 제외하고 방문한 컴퓨터의 배열 길이를 반환해야 한다.

---
+++ DFS_BFS/2606.py
@@ -0,0 +1,24 @@
+n=int(input())
+m=int(input())
+graph=[[0]*(n+1) for i in range(n+1)]
+for _ in range(m):
+    start, end=map(int, input().split())
+    #양방향이므로 양쪽에 1을 넣음
+    graph[start][end]=1
+    graph[end][start]=1
+
+def BFS(graph, root):
+    queue=[root]
+    visited=[root]
+    while queue:
+        current_node=queue.pop(0) #pop(0)이므로 제일 왼쪽거가 pop
+        for search_node in range(len(graph[current_node])):
+            #현 node의 그래프 길이 만큼 반복
+           if graph[current_node][search_node] and search_node not in visited:
+            #graph[current_node][search_node]는 0또는 1-> 간선이 있으면 1
+            #즉, 간선이 있고 search_node를 방문 안했다면 방문한걸로 바꾸고 queue에 넣음
+               queue+=[search_node]
+               visited+=[search_node]
+    return len(visited)-1 #감염된 컴퓨터를 모두 찾기 위해 BFS 이용, 루트제외 노드 수 반환
+
+print(BFS(graph, 1))
