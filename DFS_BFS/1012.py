27b8b24e39865a8700150d9721d7750e72effd78
soeun lee <thdmsdl87@gmail.com>
2020-10-02 15:43:13 +0900

    주석 수정

--- DFS_BFS/1012.py
+++ DFS_BFS/1012.py
@@ -6,11 +6,11 @@ directionx=[-1,1,0,0] #4방향중 x축
 directiony=[0,0,-1,1] #4방향중 y축
 
 def DFS(i, j):
-    # ground 전체에서 1을 확인 -> visted에 1로
+    # ground 전체에서 1을 확인 후 접근 후 -1로 수정
     ground[i][j]=-1
     for direction in range(4):
-        # 동서남북 인접한 곳에 1이 있는지 확인 -> 재귀호출로 visted에 숫자넣기
-        # 인접한 곳에 1 더이상 없으면 다음 전체 graph에서 1 확인
+        # 동서남북 인접한 곳에 1이 있는지 확인 -> 재귀호출로 ground 해당 좌표에 -1넣기
+        # 인접한 곳에 1 더이상 없으면 다음 전체 ground에 1 확인
         nearx=i+directionx[direction]
         neary=j+directiony[direction]
         if nearx<0 or nearx==m or neary<0 or neary==n:
