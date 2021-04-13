e4bc996db36870401ea736feea6e260fb29a5354
soeun lee <thdmsdl87@gmail.com>
2021-04-14 08:42:09 +0900

    21.04.13 WBS 16일차 리뷰

--- Inflearn/Section4/problem5.py
+++ Inflearn/Section4/problem5.py
@@ -8,8 +8,8 @@ meeting.sort(key=lambda x:(x[1], x[0]))
 # 첫 회의 넣고 시작, 그래서 count=1부터 시작
 count=1
 endTime=meeting[0][1]
-for i in range(1, len(meeting)):
-    if endTime<=meeting[i][0]:
+for m in meeting:
+    if endTime<=m[0]:
         count+=1
-        endTime=meeting[i][1]
+        endTime=m[1]
 print(count)
