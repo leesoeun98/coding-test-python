9d5c902462ca7898c32406ead7b608d16b1f320d
soeun lee <thdmsdl87@gmail.com>
2021-04-18 00:46:56 +0900

    21.04.17 WBS 19일차

--- Inflearn/Section6/problem5.py
+++ Inflearn/Section6/problem5.py
@@ -1,20 +1,27 @@
+# 시간초과 => 백트래킹 필요
 c, n = map(int, input().split())
-weight=[]
-result=0
-
+weight = []
+result = -987654321
 for i in range(n):
     weight.append(int(input()))
+total = sum(weight)
 
-def DFS(depth, curweight):
+
+def DFS(depth, curweight, checksum):
     global result
-    if curweight>c:
+    # 누적합을 기록해서 전체-누적합이 (앞으로 탐색 남은 애들)+현재 sum이 res보다 작으면 탐색할 이유 x
+    if curweight + (total - checksum) < result:
         return
-    if depth==n:
-        if result>curweight:
-            result=curweight
+    if curweight > c:
+        return
+    if depth == n:
+        if result < curweight:
+            result = curweight
         return
     else:
-        DFS(depth+1, curweight+weight[depth])
-        DFS(depth+1, curweight)
-DFS(0,0)
+        DFS(depth + 1, curweight + weight[depth], checksum + weight[depth])
+        DFS(depth + 1, curweight, checksum + weight[depth])
+
+
+DFS(0, 0, 0)
 print(result)
--- Inflearn/Section6/problem7.py
+++ Inflearn/Section6/problem7.py
@@ -2,6 +2,26 @@ n = int(input())
 coins=list(map(int, input().split()))
 coins.sort(reverse=True)
 change=int(input())
+res=9876543210
+def DFS(depth, sumc):
+    global res
+    if depth>res:
+        return
+    if sumc==change:
+        if res>depth:
+            res=depth
+        return
+    else:
+        for i in range(n):
+            if sumc+coins[i]<=change:
+                DFS(depth+1, sumc+coins[i])
+DFS(0,0)
+print(res)
+
+"""n = int(input())
+coins=list(map(int, input().split()))
+coins.sort(reverse=True)
+change=int(input())
 dp=[0 for i in range(change+1)]
 #초기화 주의....1부터 끝까지 해야 맨 초반에 min값이 0됨
 for i in range(1, change+1):
@@ -9,4 +29,4 @@ for i in range(1, change+1):
 for i in range(n):
     for j in range(coins[i], change+1):
         dp[j]=min(dp[j], dp[j-coins[i]]+1)
-print(dp[change])
+print(dp[change])"""
