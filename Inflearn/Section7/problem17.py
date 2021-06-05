581002d557e79730ecd00800cf07a3781a652a8e
soeun lee <thdmsdl87@gmail.com>
2021-06-05 16:40:02 +0900

    21.06.06 인프런 섹션 7 리뷰

--- Inflearn/Section7/problem17.py
+++ Inflearn/Section7/problem17.py
@@ -23,3 +23,33 @@ for l in list(combinations(pizza_store, m)):
         per_combi.append(min(per_distance_from_house))
     total_distance.append(sum(per_combi))
 print(min(total_distance))
+
+"""
+from itertools import combinations
+
+n, m = map(int, input().split())
+board=[list(map(int, input().split())) for _ in range(n)]
+house=[]
+pizza=[]
+city_distance=[]
+for i in range(n):
+    for j in range(n):
+        if board[i][j]==1:
+            house.append((i, j))
+        elif board[i][j]==2:
+            pizza.append((i, j))
+
+for combi in list(combinations(pizza, m)):
+    #각 집 당 최소 피자집 거리 구하기
+    distance_per_house=[]
+    for hx, hy in house:
+        temp=[]
+        for px, py in combi:
+            temp.append(abs(hx-px)+abs(hy-py))
+        distance_per_house.append(min(temp))
+    #피자집 거리 모두 다 더하면 도시의 피자 배달 거리
+    city_distance.append(sum(distance_per_house))
+print(min(city_distance))
+
+
+"""
